bbca2a33fa0224e569061fc3de626fc161dc0a29
alekssdz123 <alekssdzonsons89@gmail.com>
2026-05-31 00:39:08 +0300

    Added validation exception handling in GlobalExceptionHandler

--- src/main/java/firstproject/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/firstproject/demo/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,13 +26,19 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DataBaseException.class)
-        public ResponseEntity<String> handleDataBase(DataBaseException e){
-            logger.error("DataBaseException: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    public ResponseEntity<String> handleDataBase(DataBaseException e){
+        logger.error("DataBaseException: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     @ExceptionHandler(UsernameNotFoundException.class)
-        public ResponseEntity<String> handleUsernameNotFound(UsernameNotFoundException e){
-            logger.error("UsernameNotFoundException: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<String> handleUsernameNotFound(UsernameNotFoundException e){
+        logger.error("UsernameNotFoundException: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException e){
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        logger.error(message);
+        return ResponseEntity.badRequest().body(message);
+    }
 }
