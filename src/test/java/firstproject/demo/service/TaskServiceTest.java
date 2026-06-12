ad535dd7bc80cf5ee637998ccd938214b6327e83
alekssdz123 <alekssdzonsons89@gmail.com>
2026-06-12 23:58:11 +0300

    Finished tests in TaskServiceTest

--- src/test/java/firstproject/demo/service/TaskServiceTest.java
+++ src/test/java/firstproject/demo/service/TaskServiceTest.java
@@ -13,6 +13,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
     @Mock
@@ -27,7 +30,7 @@ public class TaskServiceTest {
         task.setTitle("Test title");
         task.setTaskDescription("Test description");
 
-        when(repo.save(any(Task.class))).then(invocation -> invocation.getArgument(0));
+        when(repo.save(any(Task.class))).thenAnswer(invocation -> invocation.getArgument(0));
 
         Task result = service.addTask(task);
 
@@ -36,6 +39,44 @@ public class TaskServiceTest {
         assertFalse(result.getCompleteStatus());
 
         verify(repo, times(1)).save(any(Task.class));
+    }
+
+    @Test
+    void testGetById(){
+        Task task = new Task();
+        task.setId(UUID.randomUUID());
+
+        when(repo.findById(task.getId())).thenReturn(Optional.of(task));
+
+        Task result = service.getById(task.getId());
+
+        assertEquals(task.getId(), result.getId());
+    }
+
+    @Test
+    void testDeleteTask(){
+        Task task = new Task();
+        task.setId(UUID.randomUUID());
+
+        doNothing().when(repo).deleteById(task.getId());
+
+        service.deleteTask(task.getId());
+
+        verify(repo, times(1)).deleteById(task.getId());
+
+    }
+
+    @Test
+    void testMarkAsCompleted(){
+        Task task = new Task();
+        task.setId(UUID.randomUUID());
+        task.setCompleteStatus(false);
+
+        doNothing().when(repo).markAsCompleted(task.getId());
+
+        service.markAsCompleted(task.getId());
+
+        verify(repo, times(1)).markAsCompleted(task.getId());
     }
 
 }
